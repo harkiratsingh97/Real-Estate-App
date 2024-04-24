5f21f405378a59402e357865cd75ad7c59f8d4ba
harkiratsinghh <officialharkiratsingh@gmail.com>
2024-04-24 16:25:24 +0530

    Create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,8 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
+// Controller function to Create own listing
+
 export const createListing = async (req, res, next) => {
 	try {
 		// console.log(req.body);
@@ -13,7 +15,7 @@ export const createListing = async (req, res, next) => {
 	}
 };
 
-// Controller function to delete User's own listing
+// Controller function to Delete User's own listing
 export const deleteListing = async (req, res, next) => {
 	const listing = await Listing.findById(req.params.id);
 
@@ -28,6 +30,31 @@ export const deleteListing = async (req, res, next) => {
 	res.status(200).json("Deleted");
 	try {
 		await Listing.findByIdAndDelete(req.params.id);
+	} catch (error) {
+		next(error);
+	}
+};
+
+// Controller function to update User's own listing
+export const updateListing = async (req, res, next) => {
+	const listing = await Listing.findById(req.params.id);
+
+	if (!listing) {
+		return next(errorHandler(404, "Listing not Found!"));
+	}
+
+	if (req.user.id !== listing.userRef) {
+		return next(errorHandler(401, "You can only update your own listing!"));
+	}
+
+	try {
+		const updatedListing = await Listing.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true }
+		);
+
+		res.status(200).json(updatedListing);
 	} catch (error) {
 		next(error);
 	}
