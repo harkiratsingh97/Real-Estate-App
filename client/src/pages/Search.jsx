481e44b1376d767fe9eab14581a6611845b7333b
harkiratsinghh <officialharkiratsingh@gmail.com>
2024-04-25 17:12:55 +0530

    Add Show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -18,7 +18,7 @@ const Search = () => {
 		order: "desc",
 	});
 
-	// console.log(sidebarData);
+	const [showMore, setShowMore] = useState(false);
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
@@ -52,11 +52,14 @@ const Search = () => {
 
 		const fetchListings = async () => {
 			setLoading(true);
+			setShowMore(false);
 			const searchQuery = urlParams.toString();
 			const res = await fetch(`/api/listing/get?${searchQuery}`);
 
 			const data = await res.json();
-
+			if (data.length > 8) {
+				setShowMore(true);
+			}
 			if (data.success === false) {
 				setLoading(false);
 				return;
@@ -115,6 +118,22 @@ const Search = () => {
 		const searchQuery = urlParams.toString();
 		navigate(`/search?${searchQuery}`);
 	};
+
+	const onShowMoreClick = async () => {
+		const numberOfListings = listings.length;
+		const startIndex = numberOfListings;
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("startIndex", startIndex);
+		const searchQuery = urlParams.toString();
+
+		const res = await fetch(`/api/listing/get?${searchQuery}`);
+		const data = res.json();
+		if (data.length < 9) {
+			setShowMore(false);
+		}
+		setListings([...listings, ...data]);
+	};
+
 	return (
 		<div className="flex flex-col md:flex-row">
 			<div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -236,6 +255,13 @@ const Search = () => {
 						listings.map((listing) => (
 							<ListingItem key={listing._id} listing={listing} />
 						))}
+
+					{showMore && (
+						<button
+							onClick={onShowMoreClick}
+							className="text-green-700 hover:underline p-7 w-full text-center"
+						></button>
+					)}
 				</div>
 			</div>
 		</div>
