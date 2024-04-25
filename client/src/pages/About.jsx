4e298934594d17a3c12be134fe0cdf5db0791f01
harkiratsinghh <officialharkiratsingh@gmail.com>
2024-04-25 18:14:45 +0530

    Complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    );
-}
+	return (
+		<div className="py-20 px-4 max-w-6xl mx-auto">
+			<h1 className="text-3xl font-bold mb-4 text-slate-800">
+				About Kirat Estate
+			</h1>
+			<p className="mb-4 text-slate-700">
+				Kirat Estate is a leading real estate agency that specializes in helping
+				clients buy, sell, and rent properties in the most desirable
+				neighborhoods. Our team of experienced agents is dedicated to providing
+				exceptional service and making the buying and selling process as smooth
+				as possible.
+			</p>
+			<p className="mb-4 text-slate-700">
+				Our mission is to help our clients achieve their real estate goals by
+				providing expert advice, personalized service, and a deep understanding
+				of the local market. Whether you are looking to buy, sell, or rent a
+				property, we are here to help you every step of the way.
+			</p>
+			<p className="mb-4 text-slate-700">
+				Our mission is to help our clients achieve their real estate goals by
+				providing expert advice, personalized service, and a deep understanding
+				of the local market. Whether you are looking to buy, sell, or rent a
+				property, we are here to help you every step of the way.
+			</p>
+		</div>
+	);
+};
 
 export default About;
